630c0a474488426c231e1dc0d31d1fe22aa4e9ef
exute <exute@DESKTOP-38P5TV8>
2024-10-15 18:51:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Задание 1
+            //Console.Write("число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if ( number > 0)
+            //{
+            // number++;
+            // Console.WriteLine($"Число положительное {number} ");
+            //}
+            //else
+            //{
+            // number = number - 2;
+            // Console.WriteLine($"отрицательное {number}");
+            //}
+            //Console.ReadKey();
+
+            //Задание 2
+            //Console.Write("число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 0)
+            //{
+            // number++;
+            // Console.WriteLine($"Число положительное {number} ");
+            //}
+            //else
+            //{
+            // Console.WriteLine($" число отрицательное {number}");
+            //}
+            //Console.ReadKey();
+
+            //Задание 3
+            //Console.Write("число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 0)
+            //{
+            // number++;
+            // Console.WriteLine($"Число положительное {number} ");
+            //}
+            //else if(number == 0)
+            //{
+            // number = 10;
+            // Console.WriteLine($"{number}");
+            //}
+
+            //else
+            //{
+            // number = number - 2;
+            // Console.WriteLine($"Число отрицательное {number}");
+            //}
+
+            //Console.ReadKey();
+
+            ////Задание 4
+            //Console.WriteLine("Введите число: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a % 2 == 0)
+            //{
+            // Console.WriteLine($"Число четное {a}");
+            //}
+            //else
+            //{
+            // Console.WriteLine($"Число нечетное {a}");
+            //}
+            //Console.ReadKey();
+
+            //Задание 5 Определить, является ли число положительным, отрицательным или нулевым.
+
+            //Console.WriteLine("Введите число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 0)
+            //{
+            // Console.WriteLine($"Число положительное {number}");
+            //}
+            //else if (number == 0)
+            //{
+            // Console.WriteLine($"число нулевое {number}");
+            //}
+            //else
+            //{
+            // Console.WriteLine($"число отрицательное {number} ");
+            //}
+            //Console.ReadKey();
+
+            //Задание 6 Определить, входит и цена выбранного ноутбука в диапазон от 20 до 30 тыс. рублей.
+            //Console.WriteLine("Введите число: ");
+
+            //Console.Write("x= ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 10000 && x< 20000)
+            //{
+            //    Console.WriteLine("входит");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Не входит");
+            //}
+            //Console.ReadKey();
+
+            //Console.WriteLine("Год= ");
+            //int year = int.Parse(Console.ReadLine());
+            //if (year % 4 == 0)
+            //{
+            //    Console.WriteLine($"год {year} высокостный");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($" год {year} не высокостный");
+            //}
+            //Console.ReadKey();
+
+            //double y = 0;
+            //if (x < 0)
+            //{
+            // y = Math.Pow(x,2);
+            //}
+            //else
+            //{
+            // y = Math.Pow(y, 3);
+            //}
+            //тернарный
+            //double y = x < 0 ? Math.Pow(x, 2) : Math.Pow(x, 3);
+
+
+            //Console.WriteLine(y);
+            //Console.ReadKey();
+        }
+    }
+}
